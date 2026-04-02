179723323b3b5bbd8d2860cdaf1a4de4e65380e1
rentalHolidays02 <rentalholidays.es@gmail.com>
2026-04-02 08:01:58 +0200

    Update reservas.js

--- pages/api/reservas.js
+++ pages/api/reservas.js
@@ -85,8 +85,8 @@ export default async function handler(req, res) {
       origen:        colOrigen,
       entrada:       colEntrada,
       salida:        colSalida,
-      nombre:        colNombre,
-      telefono:      colTelefono,
+      nombre:        colNOMBRE,
+      telefono:      colTELEFONO,
       observaciones: colObservacion,
       datosKiko:     colKiko,
       // Financieras
